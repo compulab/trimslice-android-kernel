637bc9aba84b61eac40f9edd883f1379418b9c99
Pavan Kunapuli <pkunapuli@nvidia.com>
2012-01-13 15:27:24 +0530

    sdhci: Add option for non std freq tuning
    Adding option for non std freq tuning for
    host controllers that have deviation in the
    tuning procedure.
    
    Bug 920089
    
    Change-Id: I8ca6962c6f0380c1160460e5094c47aee241d6e3
    Signed-off-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-on: http://git-master/r/72603
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Pradeep Goudagunta <pgoudagunta@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -264,6 +264,7 @@ struct sdhci_ops {
 	int	(*set_uhs_signaling)(struct sdhci_host *host, unsigned int uhs);
 	int	(*switch_signal_voltage)(struct sdhci_host *host,
 				unsigned int signal_voltage);
+	int	(*execute_freq_tuning)(struct sdhci_host *sdhci);
 };
 
 #ifdef CONFIG_MMC_SDHCI_IO_ACCESSORS
