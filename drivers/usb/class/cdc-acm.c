f188e37cdf1ae35d6d5ad2da473730ef0822921f
Steve Lin <stlin@nvidia.com>
2012-01-24 16:16:39 -0800

    usb: cdc-acm: Fix acm_tty_hangup() vs. acm_tty_close() race
    [Not upstream s it was fixed differently there for 3.3 with a much more
    "intrusive" rework of the driver - gregkh]
    
    There is a race condition involving acm_tty_hangup() and acm_tty_close()
    where hangup() would attempt to access tty->driver_data without proper
    locking and NULL checking after close() has potentially already set it
    to NULL.  One possibility to (sporadically) trigger this behavior is to
    perform a suspend/resume cycle with a running WWAN data connection.
    
    This patch addresses the issue by introducing a NULL check for
    tty->driver_data in acm_tty_hangup() protected by open_mutex and exiting
    gracefully when hangup() is invoked on a device that has already been
    closed.
    
    Signed-off-by: Thilo-Alexander Ginkel <thilo@ginkel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    (cherry picked from commit 53b63616b396f49775d4b2c4c3e6eab648a7833b)
    
    Bug 922026
    
    Change-Id: I36e98a3d247a0958fa9c5db984e60811e6dfe363
    Signed-off-by: Steve Lin <stlin@nvidia.com>
    Reviewed-on: http://git-master/r/77143
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sheshagiri Shenoy <sshenoy@nvidia.com>

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -705,9 +705,19 @@ static void acm_port_down(struct acm *acm)
 
 static void acm_tty_hangup(struct tty_struct *tty)
 {
-	struct acm *acm = tty->driver_data;
+	struct acm *acm;
+
+	mutex_lock(&open_mutex);
+	acm = tty->driver_data;
+
+	if (!acm)
+		goto out;
+
 	tty_port_hangup(&acm->port);
 	acm_port_down(acm);
+
+out:
+	mutex_unlock(&open_mutex);
 }
 
 static void acm_tty_close(struct tty_struct *tty, struct file *filp)
