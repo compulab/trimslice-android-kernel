a62be5b5595ad86411ced81bceeb3d6a693426d2
Bharat Nihalani <bnihalani@nvidia.com>
2012-05-04 18:36:00 +0530

    video: tegra: dc: consider bandwidth efficiency for tegra2
    This reverts commit 78361a633eb45901cb60d382c9838240407cacc4 for
    Tegra2 since the clock framework does not take the efficiency into
    account.
    
    Bug 980181
    
    Change-Id: Icba0b828550a44603f9c25ed794a2dc7781ec20d
    Signed-off-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-on: http://git-master/r/100566
    Reviewed-by: Nitin Kumbhar <nkumbhar@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Aleksandr Frid <afrid@nvidia.com>
    Reviewed-by: Dan Willemsen <dwillemsen@nvidia.com>
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -947,6 +947,14 @@ static unsigned long tegra_dc_calc_win_bandwidth(struct tegra_dc *dc,
 		(win_use_v_filter(w) ? 2 : 1) * dfixed_trunc(w->w) / w->out_w *
 		(WIN_IS_TILED(w) ? tiled_windows_bw_multiplier : 1);
 
+#ifdef CONFIG_ARCH_TEGRA_2x_SOC
+/*
+ * Assuming 60% efficiency: i.e. if we calculate we need 70MBps, we
+ * will request 117MBps from EMC.
+ */
+	ret = ret + (17 * ret / 25);
+#endif
+
 	/* if overflowed */
 	if (ret > (1UL << 31))
 		return ULONG_MAX;
