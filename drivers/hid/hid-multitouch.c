52094ab09b688135f27ea028ae9eebab4160c587
Valentin Raevsky <valentin@compulab.co.il>
2012-05-24 17:06:10 +0300

    tegra: trimslice: touchscreen: Add Baanto SDW-190S1-M5T-EE1-S1-PRD hid-multitouch support

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -747,6 +747,11 @@ static const struct hid_device_id mt_devices[] = {
 		HID_USB_DEVICE(USB_VENDOR_ID_XAT,
 			USB_DEVICE_ID_XAT_CSR) },
 
+	/* Baanto */
+	{ .driver_data = MT_CLS_DEFAULT,
+		HID_USB_DEVICE(USB_VENDOR_ID_BAANTO,
+			USB_DEVICE_ID_BAANTO) },
+
 	{ }
 };
 MODULE_DEVICE_TABLE(hid, mt_devices);
